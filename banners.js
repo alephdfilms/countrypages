ee3743a31bc27a717a717bc5da394d2313514ea8
Aleph Diallo <alephdiallo@mail.com>
2023-06-03 10:19:12 -0400

    Update banners.js

--- banners.js
+++ banners.js
@@ -1,15 +1,7 @@
-// Random Banners
-var ads = 1;
-
-function random_imglink(){
-var myimages=new Array(ads)
-myimages[1]="https://alephdfilms.github.io/countrypages/pictures/advertisements/an_hero.gif"
-
-var alttag=new Array(ads)
-alttag[1]="Why not become an hero?"
-
-var imagelinks=new Array(ads)
-imagelinks[1]="https://alephdfilms.github.io/countrypages/residents/anonymous/anhero/index.html"
-
-var y=Math.round(Math.random()*ads)%ads+1;
-document.write('<a href="'+imagelinks[y]+'"><img src="'+myimages[y]+'" width="468" height="60" border="0" title="'+alttag[y]+'" alt="'+alttag[y]+'"></a><br /><a href="'+imagelinks[y]+'">'+alttag[y]+'</a>')
+var images = [], 
+	index = 0;
+	//Here come the ads! get ready!!!
+  images[0] = "<a href='https://alephdfilms.github.io/countrypages/residents/42ndstreet/an_hero/index.html'><img src='https://alephdfilms.github.io/countrypages/pictures/advertisements/an_hero.gif' alt='Why not become an hero?'></a>";
+  //Thasall. Thanks for participating.
+index = Math.floor(Math.random() * images.length);
+document.write(images[index]);
