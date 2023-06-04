7e2de8d6bd385ce9f4f134aefc7ce3fe0fccb6f1
Aleph Diallo <alephdiallo@mail.com>
2023-06-04 11:30:56 -0400

    Update banners.js
    fixed links

--- banners.js
+++ banners.js
@@ -1,7 +1,7 @@
 var images = [], 
 	index = 0;
 	//Here come the ads! get ready!!!
-  images[0] = "<a href='https://countrypages.github.io/countrypages/residents/42ndstreet/an_hero/index.html'><img src='https://countrypages.github.io/countrypages/pictures/advertisements/an_hero.gif' alt='Why not become an hero?'></a>";
+  images[0] = "<a href='/countrypages/residents/42ndstreet/an_hero/index.html'><img src='/countrypages/pictures/advertisements/an_hero.gif' alt='Why not become an hero?'></a>";
   //Thasall. Thanks for participating.
 index = Math.floor(Math.random() * images.length);
 document.write(images[index]);
