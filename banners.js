ff42f2caeaa3add5b34f5f846e72b22918088166
Aleph Diallo <alephdiallo@mail.com>
2023-06-04 11:33:45 -0400

    Update banners.js
    I feel like a dumbass for leaving the /countrypages/ part of the link in.

--- banners.js
+++ banners.js
@@ -1,7 +1,7 @@
 var images = [], 
 	index = 0;
 	//Here come the ads! get ready!!!
-  images[0] = "<a href='/countrypages/residents/42ndstreet/an_hero/index.html'><img src='/countrypages/pictures/advertisements/an_hero.gif' alt='Why not become an hero?'></a>";
+  images[0] = "<a href='/residents/42ndstreet/an_hero/index.html'><img src='/pictures/advertisements/an_hero.gif' alt='Why not become an hero?'></a>";
   //Thasall. Thanks for participating.
 index = Math.floor(Math.random() * images.length);
 document.write(images[index]);
